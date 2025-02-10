6e2d6321baa4d5e705c8c9e74143caf5d79f1039
BohdanVoznyi <bohdanvoznyi.em@gmail.com>
2025-02-10 08:55:48 +0200

    craete user with prisma

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { CreateUserRequest } from './dto/create-user.request';
+import { PrismaService } from '../prisma/prisma.service';
+import { User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  async createUser(request: CreateUserRequest) {
-    console.log(request);
+  constructor(private readonly prismaService: PrismaService) {
+  }
+
+  async createUser(data: CreateUserRequest):Promise<Omit<User, 'password'>> {
+    try {
+      return await this.prismaService.user.create({
+        data: {
+          ...data,
+          password: await bcrypt.hash(data.password, 10),
+        },
+        select: {
+          email: true,
+          id: true,
+        }
+      });
+    } catch (err) {
+      if (err.code === 'P2002') {
+        throw new UnprocessableEntityException('Email already exists');
+      }
+      throw err;
+    }
   }
 }
