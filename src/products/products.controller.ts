78f00f2f8381e91dda4d2757912acd26e7111e93
Bohdan Voznyi <bohdanvoznyi.em@gmail.com>
2025-02-18 13:00:51 +0200

    upload product img

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,9 +1,24 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  FileTypeValidator,
+  Get,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { CreateProductRequest } from './dto/create-product.request';
 import { TokenPayload } from '../auth/interfaces/token-payload.interface';
 import { CurrentUser } from '../auth/decorators/current-user.decorator';
 import { ProductsService } from './products.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import e from 'express';
+import { extname } from 'path';
 
 @Controller('products')
 export class ProductsController {
@@ -11,9 +26,40 @@ export class ProductsController {
 
   @Post()
   @UseGuards(JwtAuthGuard)
-  async createProduct(@Body() body: CreateProductRequest, @CurrentUser() user: TokenPayload) {
+  async createProduct(
+    @Body() body: CreateProductRequest,
+    @CurrentUser() user: TokenPayload,
+  ) {
     return this.productsService.createProduct(body, user.userId);
   }
+
+  @Post(':productId/image')
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: 'public/products',
+        filename(
+          req: e.Request,
+          file: Express.Multer.File,
+          callback: (error: Error | null, filename: string) => void,
+        ) {
+          callback(null, `${req.params.producId}${extname(file.originalname)}`);
+        },
+      }),
+    }),
+  )
+  async uploadProductImage(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 500_000 }),
+          new FileTypeValidator({ fileType: 'image/png' }),
+        ],
+      }),
+    )
+    _file: Express.Multer.File,
+  ) {}
 
   @Get()
   @UseGuards(JwtAuthGuard)
