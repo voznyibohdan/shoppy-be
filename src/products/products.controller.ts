2992fe5763e319eca8a98ab71208b88d96bfd2f0
Bohdan Voznyi <bohdanvoznyi.em@gmail.com>
2025-02-18 09:46:56 +0200

    product model

---
+++ src/products/products.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { CreateProductRequest } from './dto/create-product.request';
+import { TokenPayload } from '../auth/interfaces/token-payload.interface';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
+import { ProductsService } from './products.service';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  async createProduct(@Body() body: CreateProductRequest, @CurrentUser() user: TokenPayload) {
+    return this.productsService.createProduct(body, user.userId);
+  }
+}
